bba7cf46640f39ac059584d8c266875954e1909d
Doxin <lieuwemo@gmail.com>
2021-05-30 20:55:33 +0200

    Add workflow to generate linux binary release

---
+++ entry.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from src.main import main  # noqa nosort
+
+main()
--- src/__main__.py
+++ src/__main__.py
@@ -1,16 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
-import os.path
+from .main import main
 
-from .gui import ChatWindow
-
-from . import versions  # noqa nosort
-from . import protocol  # noqa nosort
-
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
-w = ChatWindow()
-w.connect("destroy", lambda e: protocol.stop())  #  TODO: minimize to tray?
-w.show_all()
-
-protocol.start()
+main()
